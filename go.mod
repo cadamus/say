88df196819dc1144f3f4a1b7029bbde9f5322e4d
Alan Mills <alan.mills@cadamus.com>
2024-02-18 17:40:33 +0000

    feat: First version of say module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cadamus/say
+
+go 1.22.0
---
+++ say.go
@@ -0,0 +1,7 @@
+// Package say returns friendly statements
+package say
+
+// Hello returns the famous "hello, World!" greeting
+func Hello() string {
+	return "hello, World!"
+}
